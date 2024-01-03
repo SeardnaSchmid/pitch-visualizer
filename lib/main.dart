c8f657a96f34701331cefa432bc5cea7a22df56e
andre <as@as.com>
2024-01-03 01:31:42 +0100

    using record

--- lib/main.dart
+++ lib/main.dart
@@ -1,109 +1,35 @@
+import 'package:flutter/foundation.dart';
 import 'package:flutter/material.dart';
-import 'package:flutter_sound/flutter_sound.dart';
-import 'package:permission_handler/permission_handler.dart';
-import 'package:path_provider/path_provider.dart';
 
-void main() {
-  runApp(MyApp());
+import 'recorder.dart';
+
+void main() => runApp(const MyApp());
+
+class MyApp extends StatefulWidget {
+  const MyApp({super.key});
+
+  @override
+  State<MyApp> createState() => _MyAppState();
 }
 
-class MyApp extends StatelessWidget {
-  const MyApp({Key? key}) : super(key: key); // Added named 'key' parameter
+class _MyAppState extends State<MyApp> {
+  String? audioPath;
+
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
-      home: MyHomePage(),
-    );
-  }
-}
-
-class MyHomePage extends StatefulWidget {
-  const MyHomePage({Key? key}) : super(key: key); // Added named 'key' parameter
-  @override
-  _MyHomePageState createState() => _MyHomePageState();
-}
-
-class _MyHomePageState extends State<MyHomePage> {
-  FlutterSoundRecorder? _recorder;
-  bool _isRecording = false;
-
-  @override
-  void initState() {
-    super.initState();
-    _recorder = FlutterSoundRecorder();
-    _initRecorder();
-  }
-
-  Future<void> _initRecorder() async {
-    var status = await Permission.microphone.request();
-    if (status.isGranted) {
-      await _recorder!.openAudioSession();
-    } else {
-      if (mounted) {
-        // Check if the widget is still mounted
-        showDialog(
-          context: context,
-          builder: (BuildContext context) {
-            return AlertDialog(
-              title: const Text('Microphone Permission'),
-              content: const Text('Microphone permission is not granted'),
-              actions: <Widget>[
-                TextButton(
-                  child: const Text('OK'),
-                  onPressed: () {
-                    Navigator.of(context).pop();
-                  },
-                ),
-              ],
-            );
-          },
-        );
-      }
-    }
-  }
-
-  Future<void> _startRecording() async {
-    var tempDir = await getTemporaryDirectory();
-    var filePath = '${tempDir.path}/my_recording.aac';
-    await _recorder!.startRecorder(toFile: filePath);
-    setState(() {
-      _isRecording = true;
-    });
-  }
-
-  Future<void> _stopRecording() async {
-    await _recorder!.stopRecorder();
-    setState(() {
-      _isRecording = false;
-    });
-  }
-
-  @override
-  Widget build(BuildContext context) {
-// Store the BuildContext in a variable
-    return Scaffold(
-      appBar: AppBar(
-        title: const Text('Audio Recorder'),
-      ),
-      body: Center(
-        child: Column(
-          mainAxisAlignment: MainAxisAlignment.center,
-          children: <Widget>[
-            const Text('Press the button to start recording'),
-            IconButton(
-              icon: Icon(_isRecording ? Icons.stop : Icons.mic),
-              onPressed: _isRecording ? _stopRecording : _startRecording,
-            ),
-          ],
+      home: Scaffold(
+        body: Center(
+          child: Recorder(
+            onStop: (path) {
+              if (kDebugMode) print('Recorded file path: $path');
+              setState(() {
+                audioPath = path;
+              });
+            },
+          ),
         ),
       ),
     );
-  }
-
-  @override
-  void dispose() {
-    _recorder!.closeAudioSession();
-    _recorder = null;
-    super.dispose();
   }
 }
